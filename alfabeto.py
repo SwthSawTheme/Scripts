88b1e2f77c67cc023b768bb77aef041ecbed902a
SwthSawTheme <sawtheme1@gmail.com>
2024-07-31 00:13:17 -0300

    melhoria e legibilidade do código

--- alfabeto.py
+++ alfabeto.py
@@ -2,11 +2,26 @@
 import string
 
 def alfabeto(num:int):
-    if num >= 1 and num <= 26:
-        alfa = list(string.ascii_uppercase[:num])
-        return alfa
-    else:
-        return Exception("Digite um inteiro entre 1 e 26")
+    """
+    Retorna um valor representado nas letras do alfabeto.
 
-num = int(input())
-print(alfabeto(num))
+    Args:
+    num (int): um número inteiro entre 1 e 26.
+
+    Returns:
+    list(str): uma lista de letras do alfabeto até o número fornecido.
+
+    Raises:
+    ValueError: se o número não estiver dentro do intervalo de 1 e 26.
+    """
+    if num >= 1 and num <= 26:
+        return list(string.ascii_uppercase[:num])
+    else:
+        raise ValueError("Digite um inteiro entre 1 e 26")
+
+
+try:
+    num = int(input())
+    alfabeto(num)
+except ValueError as e:
+    print(e)
