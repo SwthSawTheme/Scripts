7a97baf252d5bfb8c2ec96d25b9c12d2e6f2dcb1
SwthSawTheme <sawtheme1@gmail.com>
2024-07-30 23:52:20 -0300

    Exercicio resolvido, abrange verificação de intervalo, retorno de função, verificação de retorno

--- alfabeto.py
+++ alfabeto.py
@@ -2,11 +2,11 @@
 import string
 
 def alfabeto(num:int):
-    if num > 1 and num < 26:
+    if num >= 1 and num <= 26:
         alfa = list(string.ascii_uppercase[:num])
-        return print(alfa)
+        return alfa
     else:
         return Exception("Digite um inteiro entre 1 e 26")
 
 num = int(input())
-alfabeto(num)
+print(alfabeto(num))
