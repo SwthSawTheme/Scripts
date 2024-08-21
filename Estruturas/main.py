0c52c34774b7298e8baa69417fb45d56906221c2
SwthSawTheme <sawtheme1@gmail.com>
2024-08-21 00:14:43 -0300

    Exercicio 5

--- Estruturas/main.py
+++ Estruturas/main.py
@@ -3,18 +3,25 @@
 # Exercício 1: Crie uma lista que contenha os números de 1 a 10 e imprima todos os números.
 
 lista = [1,2,3,4,5,6,7,8,9,10]
-print(lista)
+#print(lista)
 
 # Exercício 2: Adicione os números 11 e 12 ao final da lista e imprima a lista atualizada.
 lista.append(11)
 lista.append(12)
-print(lista)
+#print(lista)
 
 # Exercício 3: Remova o número 5 da lista e imprima a lista.
 lista.remove(5)
-print(lista)
+#print(lista)
 
 # Exercício 4: Inverta a ordem da lista e imprima o resultado.
 
 lista.reverse()
-print(lista)
+# print(lista)
+
+# Exercício 5: Encontre o maior e o menor número na lista.
+
+menor = min(lista)
+maior = max(lista)
+
+print(menor,maior)
