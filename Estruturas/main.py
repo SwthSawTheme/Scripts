4f300538d869047e7979800ed48a104e2d85022d
SwthSawTheme <sawtheme1@gmail.com>
2024-08-21 00:07:37 -0300

    Exercicio 1

---
+++ Estruturas/main.py
@@ -0,0 +1,8 @@
+# Estruturas de Dados para iniciantes
+
+# Exercício 1: Crie uma lista que contenha os números de 1 a 10 e imprima todos os números.
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(lista)
+
+#
