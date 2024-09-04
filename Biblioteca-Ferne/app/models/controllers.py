fa3231bdcbca617fe76ff5ab92bcc8ac4bd53de2
SwthSawTheme <sawtheme1@gmail.com>
2024-09-04 19:59:44 -0300

    Class Livro criada e testada

---
+++ Biblioteca-Ferne/app/models/controllers.py
@@ -0,0 +1,7 @@
+titulo = " Teste"
+autor = "it"
+ano_publicacao = "coisa"
+formato = [f"Titulo:{titulo}",f"Autor:{autor}",f"Ano de Publicação:{ano_publicacao}"]
+
+for i in formato:
+    print(i)
--- Biblioteca-Ferne/app/models/livro.py
+++ Biblioteca-Ferne/app/models/livro.py
@@ -1,2 +1,24 @@
 
 
+class Livro(object):
+
+    def __init__(self,titulo:str,autor:str,ano_publicacao:str):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_publicacao = ano_publicacao
+
+    def getBook(self):
+        formato = [f"Titulo: {self.titulo}",
+                   f"Autor: {self.autor}",
+                   f"Publicação:{self.ano_publicacao}"
+                   ]
+        
+        for l in formato:
+            print(l)
+
+
+itAcoisa = Livro("It - A Coisa","Stephan King"," fev de 1999")
+
+
+if __name__ == "__main__":
+    itAcoisa.getBook()
