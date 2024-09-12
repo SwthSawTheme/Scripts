a09e8ccfbc1ed007e6d32dbc5f0b4d9cd6d86d77
SwthSawTheme <sawtheme1@gmail.com>
2024-09-12 20:54:02 -0300

    Desafio função retornar se um número é par ou impar concluido

---
+++ DESAFIOS/function.py
@@ -0,0 +1,12 @@
+
+def getParImpar(num):
+    if num % 2 == 0:
+        print("Par")
+    else:
+        print("Impar")
+
+if __name__ == "__main__":
+    getParImpar(2)
+    getParImpar(3)
+    getParImpar(4)
+    getParImpar(5)
