f3b43febcbcfef378b32326bf51845086b5f8c0b
SwthSawTheme <sawtheme1@gmail.com>
2025-01-03 21:59:03 -0300

    teste de criptografia mal sucedido

---
+++ test/main.py
@@ -0,0 +1,17 @@
+import time
+import random
+
+
+def cryptofy(key:list):
+    new_key = []
+    for item in key:
+        if item == str():
+            item +="abnd"
+        elif item == int():
+            item += "124"
+        new_key.append(item)
+    return new_key
+
+lista = [1,24,566,"sfnhjks0","1248742jhkfs",124,53,5555,"tjkb"]
+test = cryptofy(list)
+print(test)        
