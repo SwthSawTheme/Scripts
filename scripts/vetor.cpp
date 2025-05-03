aab12d060215b044bd033e999f3af251c6102129
SwthSawTheme <sawtheme1@gmail.com>
2025-05-02 21:07:13 -0300

    Melhoria na exibição das informações do exercicio

--- scripts/vetor.cpp
+++ scripts/vetor.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main(){
     
     vector<int> vetor;
+    vector<int> par;
+    vector<int> impar;
     int countPar = 0;
     int countImpar = 0;
 
-    for (int i=0; i < 6;i++){
+    for (int i=0; i < 6; i++){
         int valor;
         cout << "Insira um valor: " << endl;
         cin >> valor;
@@ -19,13 +21,21 @@ int main(){
     for (int i=0; i < vetor.size(); i++){
         if (vetor[i] % 2 == 0){
             countPar ++;
+            par.push_back(vetor[i]);
         } else {
             countImpar ++;
+            impar.push_back(vetor[i]);
         }
     }
 
     cout << "Quantidade de numeros pares: " << countPar << endl;
+    for (int i = 0; i < par.size(); i++){
+        cout << "Numeros pares: " << par[i] << endl;
+    }
     cout << "Quantidade de numeros impares: " << countImpar << endl;
+    for (int i = 0; i < impar.size(); i++){
+        cout << "Numeros impares: " << impar[i] << endl;
+    }
 
     return 0;
 }
